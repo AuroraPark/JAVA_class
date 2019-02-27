dc105bf3e8012e2ca6211ef38d475b0b4b5425a2
AuroraPark <a20190212@gmail.com>
2019-02-27 19:18:07 +0900

    day01 - HelloWorld & 별찍기

---
+++ day01/src/day1/Hello.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Hello {
+	public static void main(String[] args) {
+		// sysout 입력하고, ctrl+space 누르면 자동 완성됨.
+		System.out.println("Hello");
+		
+		// PRINT
+		System.out.println("안녕하세요.");
+		System.out.println("반갑습니다.");
+		System.out.println("1+3");
+		System.out.println(1+3);
+		
+		// 변수
+		int x = 3;	// 변수 x에 3을 저장
+		int y = 5;	// 변수 y에 5를 저장
+	}
+}
