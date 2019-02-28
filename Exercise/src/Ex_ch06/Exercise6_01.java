e9a2e4abccd1668bb6f566d9b56e102ecf389184
AuroraPark <a20190212@gmail.com>
2019-02-28 17:35:22 +0900

    day06 객체지향

--- Exercise/src/Ex_ch06/Exercise6_01.java
+++ Exercise/src/Ex_ch06/Exercise6_01.java
@@ -2,14 +2,32 @@
  * 
 */
 package Ex_ch06;
-class SutdaCard {
-	int num = 0; // 카드의 숫자 1~10사이의 정수
-	boolean isKwang = true; //광이면 true, 아니면 false
-	
-}
+
 public class Exercise6_01 {
 	public static void main(String[] args) {
+		SutdaCard card1 = new SutdaCard(3, false);	//3
+		SutdaCard card2 = new SutdaCard();		//1K
+
+		System.out.println(card1.info()); // 3
+		System.out.println(card2.info()); // 1K
 
 	}
+}
 
+class SutdaCard {
+	int num; // 카드의 숫자 1~10사이의 정수
+	boolean isKwang; // 광이면 true, 아니면 false
+
+	SutdaCard() {
+		this(1, true);	// 1K
+	} // 기본
+
+	SutdaCard(int num, boolean isKwang) {	// 3
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+
+	String info() {
+		return num + (isKwang ? "K" : "");
+	}
 }
---
+++ Exercise/src/Ex_ch06/Exercise6_03.java
@@ -0,0 +1,18 @@
+package Ex_ch06;
+
+class Student {
+	String name; // 학생 이름
+	int ban;	// 반
+	int no;		// 번호
+	int kor;	// 국어점수
+	int eng; 	// 영어점수
+	int math;	// 수학점수
+}
+public class Exercise6_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
