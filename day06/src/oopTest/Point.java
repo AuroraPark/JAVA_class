c14a461793faf579d48be7ad2601219df18c7971
AuroraPark <a20190212@gmail.com>
2019-03-04 18:01:16 +0900

    day06 내가 푼 oopTest

--- day06/src/oopTest/Point.java
+++ day06/src/oopTest/Point.java
@@ -1,98 +1,106 @@
-// 코드 여기에 추가
+// 코드 여기에 추가 - 답안
 
 package oopTest;
 
-// 1-1
+// 1-1  두개의 정수 x, y를 저장하기 위한 인스턴스 변수가 선언된 Point클래스를 정의하시오.
 public class Point {
 
-	int x;
-	int y;
+	int x; // 원래 이름 this.x
+	int y; // 원래 이름 this.y
 
-	// 1-3
+//  1-7 Point 클래스의 equals()를 다음과 같은 조건으로 오버라이딩 하고 테스트  하시오
+//
+//  - 비교하는 객체가 Point객체가 아니면 false를 반환
+//  - Point객체의 x와 y의 값이 같을 때만 true를 반환
+	// 두점이 같으면 true,다르면 false
+	// 같다는건, x와 비교 대상의 x가 같고, y와 비교 대상의 y가 같은 경우
+
+	public boolean equals(Object obj) {
+//	1. 비교하는 객체가 Point객체가 아니면 false를 반환
+		if (!(obj instanceof Point))
+			return false;
+
+		// 형변환을 통해 사용할 수 있게 만든다!
+		Point p = (Point) obj;
+
+//	2. Point객체의 x와 y의 값이 같을 때만 true를 반환
+		return this.x == p.x && this.y == p.y;
+		// 간단하게 한줄로 쓸 수 있다.
+//		if (this.x == p.x && this.y == p.y)
+//			return true;
+//		else
+//			return false;
+	}
+
+//	1-3 Point클래스에 toString()을 오버라이딩 해서 x와 y의 값을 포함한 문자열을 반환 하도록 하시오.
+//	PointTest클래스에서 Point객체를 생성한 다음, x와 y의 값을 3과 5로 초기화 하시오.
+//	그리고 toString()을 호출해서 x와 y의 값을 출력하시오 .
 	public String toString() {
 		return "toString> x : " + this.x + ", y : " + this.y;
 	}
 
-	// 1-4
-	Point() {
-		this(1, 1);
+//	 1-5 Point 클래스에 두 점 사이의 거리를 double 타입의 값으로 계산해서 
+//	반환하는 static 메서드 getDistance(Point p1, Point p2)를 추가하시오.
+//	public static double getDistance(int x1, int y1, int x2, int y2) { //를 묶은것
+	public static double getDistance(Point p1, Point p2) { // cm
+		int a = p1.x - p2.x;
+		int b = p1.y - p2.y;
+
+		return Math.sqrt(a * a + b * b);
 	}
 
+//	1-6 Point 클래스에 두 점 사이의 거리를 double타입의 값으로 계산해서 
+//	반환하는  getDistance(Point p)를 추가하시오.
+	double getDistance(Point p) { // im
+//		double a = this.x - p.x;	
+//		double b = this.y - p.y;
+//
+//		return Math.sqrt(a * a + b * b);
+
+		return getDistance(this, p); // 내용이 같으면 (im -호출- cm)으로 중복 제거
+	}
+
+//	 1-4 Point 클래스에 x,y를 매개변수로 하는 생성자와 기본 생성자(x,y를 모두 1로 초기화)를 추가하시오. 
+//	그 다음에 PointTest클래스에서 테스트 하시오.
+
+	// 매개변수가 있는 생성자
 	Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	// 1-5
-	public static double getDistance(Point p1, Point p2) {
-		double a = p1.x - p2.x;
-		double b = p1.y - p2.y;
-
-		return Math.sqrt(a * a + b * b);
-	}
-
-	// 1-6
-	double getDistance(Point p) {
-		double a = this.x - p.x;	
-		double b = this.y - p.y;
-
-		return Math.sqrt(a * a + b * b);
-	}
-
-	// 1-7
-	
-	// 두점이 같으면 true,다르면 false
-	// 같다는건, x와 비교 대상의 x가 같고, y와 비교 대상의 y가 같은 경우
-	
-public boolean equals(Object obj) {
-		// obj를 이용해서 p2의 x,y를 출력
-		// p2 의 주소를 참조변수를 통해 가져온다. (X) 형변환 필요
-		// Object (참조형) 의 매개변수 obj
-		// obj 는 Point(x,y) 형변환 필요
-		// p.equals 는 
-		int a =((Point)obj).x;
-		int b =((Point)obj).y;
-		
-		if (obj instanceof Point) {
-			// Point 객체인지 
-			if (this.x ==  a) {
-				if(this.y == b) {
-				return true;
-				} else {
-					return false;
-				}
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
+	// 기본생성자
+	Point() {
+//		x = 1;
+//		y = 1;
+		this(1, 1); // Point(1, 1)
 	}
 
 }
 
-//1-8
+//	1-8 Point클래스를 상속받고 z를 인스턴스 변수로 갖는 Point3D 클래스를 작성하시오.
 class Point3D extends Point {
 	int z;
 
-	// 1-9
+//	1-9 Point3D클래스에 생성자 Point3D(int x, int y, int z) 와 
+//	기본 생성자 (x,y,z를 1로 초기화)를 추가하시오.
 	Point3D() {
+	//	Point3D(1,1,1); // 오류
 		this(1, 1, 1);
 	}
 
 	Point3D(int x, int y, int z) {
-
+		super(x, y); // 조상의 멤버는 조상의 생성자가 초기화 하게 한다.
 		this.z = z;
 	}
 
-	// 1-10
+//  1-10 Point3D클래스의 toString()이 x,y,z 의 값을 
+//	문자열로 반환하도록 오버라이딩 하시오.
 	public String toString() {
-		return "toString> x : " + super.x + ", y : " + super.y + ", x : " + this.z;
+//		return "toString> x : " + super.x + ", y : " + super.y + ", z : " + this.z;
+//		// 중복 제거
+		
+		return super.toString() + ", z : " + this.z;	// 조상 거 가져다가 쓰기
 	}
 }
 
-
-/* 오브젝트 클래스의 메서드 오버라이딩
-public boolean equals(Object obj)
-Public String toString()
-*/
--- day06/src/oopTest/PointTest.java
+++ day06/src/oopTest/PointTest.java
@@ -1,28 +1,35 @@
-// 테스트
+// 테스트 - 답안
 
 package oopTest;
 
-//1-2
+//1-2  앞서 정의한 Point클래스를 테스트 하기위한
+// PointTest 클래스를 작성하고, 
+// Point객체를 생성하는 코드를 작성하시오.
 public class PointTest {
 
 	public static void main(String[] args) {
-		Point p = new Point(5,3);
-		Point p2 = new Point(3,4);
-
-		System.out.println(p.x);
-		System.out.println(p.y);
+		// 2
+//		Point p = new Point();
+//		p.x = 5;
+//		p.y = 3;
+		
+		// 4
+		Point p = new Point();
+		Point p2 = new Point(0,0);
+		Point3D p3 = new Point3D(1,3,2);
+		
+		System.out.println(p);
+		// 3 테스트
 		System.out.println(p.toString());
+		// 5 테스트 - 클래스 메서드(cm) / 클래스이름.메소드
+		// p와 p2의 거리를 계산
 		System.out.println(Point.getDistance(p, p2));
-		
+		// 6 테스트 - 인스턴스 메서드(im)
+		// p와 p2의 거리를 계산(나하고 p2)
 		System.out.println(p.getDistance(p2));
-		
-		System.out.println("equals:"+p.equals(p2));
-
-		Point3D p3 = new Point3D();
-		System.out.println("p3.x : " + p3.x);
-		System.out.println("p3.y : " + p3.y);
-		System.out.println("p3.z : " + p3.z);
-		System.out.println("p3.toString() : " +p3.toString());
+		// 7 테스트
+		System.out.println(p.equals(p2));
+		// Point3D
+		System.out.println(p3.toString());
 	}
-
 }
