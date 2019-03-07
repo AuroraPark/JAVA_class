727d58f105dd64a624114fe0420092a3700d36a3
AuroraPark <a20190212@gmail.com>
2019-03-07 18:52:20 +0900

    day01 - HelloWorld & 별찍기

---
+++ day01/src/day1/Ex_print3.java
@@ -0,0 +1,36 @@
+/* 반복문 - {} 안의 문장들을 반복한다.
+ * 1부터 5까지 5번 반복
+ * for (int i = 1; i <= 5; i++) {}
+ * println()은 출력후 줄바꿈
+ * print()는 출력후 줄바꿈 안함
+ * 블럭설정 후 alt+화살표 키 하면 이동됨
+ */
+package day1;
+
+public class Ex_print3 {
+
+	public static void main(String[] args) {
+		// 3
+		System.out.println("------------3----------");
+
+		for (int i = 5; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			System.out.println("*");
+		}                                                                                       
+
+		// if문 사용 방식
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (i + j == 4)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ day01/src/day1/Ex_print8.java
@@ -0,0 +1,28 @@
+/* 반복문 - {} 안의 문장들을 반복한다.
+ * 1부터 5까지 5번 반복
+ * for (int i = 1; i <= 5; i++) {}
+ * println()은 출력후 줄바꿈
+ * print()는 출력후 줄바꿈 안함
+ * 블럭설정 후 alt+화살표 키 하면 이동됨
+ */
+package day1;
+
+public class Ex_print8 {
+
+	public static void main(String[] args) {
+
+		// 8
+		System.out.println("------------8----------");
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5 + i; j++) {
+				if (i + j >= 4)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
