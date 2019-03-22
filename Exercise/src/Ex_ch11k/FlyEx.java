d9c8b2e5ccc55ecedd6d430c628c0edbfd715747
AuroraPark <a20190212@gmail.com>
2019-03-22 17:57:32 +0900

    fly알고리즘!

---
+++ Exercise/src/Ex_ch11k/FlyEx.java
@@ -0,0 +1,10 @@
+package Ex_ch11k;
+
+public class FlyEx {
+	public static void main(String[] args) {
+		// 개수(num), 시작위치(x), 끝위치(y)를 받아온다
+		// 개수(num)만큼 테스트를 반복한다.
+		// 끝위치(y)-시작위치(x) 로 거리(distance)를 구한다.
+	}
+
+}
